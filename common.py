ac469b5cb286b078eafa72e4f7905df9a63ead94
Pinky Gautam <pinky@ddproperty.com>
2023-12-30 22:37:04 +0700

    Update common.py

--- common.py
+++ common.py
@@ -6,13 +6,22 @@ from confluent_kafka.admin import AdminClient, NewTopic
 
 load_dotenv()
 
-schema_registry_conf = {"url": "localhost:8081"}
+schema_registry_conf = {"url": "http://localhost:8081"}
 kafka_server_conf = {
     "bootstrap.servers": "localhost:9092,localhost:9093,localhost:9094",
     "security.protocol": "SASL_SSL",
     "sasl.mechanism": "PLAIN",
     "sasl.username": "client",
     "sasl.password": os.environ.get("USER_CLIENT"),
+}
+producer_conf = {
+    **kafka_server_conf,
+    "ssl.truststore.location": os.environ.get("TRUST_STORE"),
+    "ssl.truststore.password": os.environ.get("P12_PASS"),
+}
+admin_conf = {
+    **kafka_server_conf,
+    "ssl.ca.location": os.environ.get("CA_CERT"),
 }
 topic = "users"
 
@@ -47,7 +56,7 @@ data_class = {"v1": UserV1, "v2": UserV2, "v3": UserV3}
 
 
 def create_topic(topic: str):
-    admin = AdminClient(kafka_server_conf)
+    admin = AdminClient(admin_conf)
     topics = admin.list_topics().topics
     if topic in topics:
         return

5e81cbc64bcce342ff1aee89e2a6873ef5b89144
Pinky Gautam <pinky@ddproperty.com>
2024-01-02 22:35:12 +0700

    Update registry.sh

--- init/registry.sh
+++ init/registry.sh
@@ -1,11 +1,15 @@
-SR_CA=/etc/schema-registry/kafka.client.truststore.pkcs12
+# define var of reuse
+SR_CA=/etc/schema-registry/$SR_HOST.truststore.pkcs12
 export SCHEMA_REGISTRY_KAFKASTORE_SSL_TRUSTSTORE_LOCATION=$SR_CA
 export SCHEMA_REGISTRY_KAFKASTORE_SSL_TRUSTSTORE_PASSWORD=$P12_PASS
 export SCHEMA_REGISTRY_KAFKASTORE_SASL_JAAS_CONFIG="org.apache.kafka.common.security.plain.PlainLoginModule required \ username=\"client\" \ password=\"${USER_CLIENT}\";"
-
+export SCHEMA_REGISTRY_SSL_KEYSTORE_LOCATION=/etc/schema-registry/secrets/$SR_HOST.keystore.pkcs12
+export SCHEMA_REGISTRY_SSL_KEYSTORE_PASSWORD=$P12_PASS
+export SCHEMA_REGISTRY_SSL_KEY_PASSWORD=$P12_PASS
 export PASSWORD_FILE=/etc/schema-registry/passwd
 export JAAS_CONFIG=/etc/schema-registry/schema-registry.jaas
 
+# save auth config to files
 echo "client: $SR_PASSWORD,developer" > $PASSWORD_FILE
 echo "
 SchemaRegistry-Props {
@@ -19,9 +23,21 @@ SchemaRegistry-Props {
 # set auth config env vars
 export SCHEMA_REGISTRY_AUTHENTICATION_METHOD=BASIC
 export SCHEMA_REGISTRY_AUTHENTICATION_ROLES=admin,developer
-export AUTHENTICATION_REALM=SchemaRegistry-Props
+export SCHEMA_REGISTRY_AUTHENTICATION_REALM=SchemaRegistry-Props
 export SCHEMA_REGISTRY_OPTS=-Djava.security.auth.login.config=$JAAS_CONFIG
 
+# create sr key store and import cert
+keytool -delete -noprompt -keystore $SCHEMA_REGISTRY_SSL_KEYSTORE_LOCATION -storepass $P12_PASS
+keytool -importkeystore \
+-deststorepass $P12_PASS \
+-destkeystore $SCHEMA_REGISTRY_SSL_KEYSTORE_LOCATION \
+-srckeystore /etc/schema-registry/secrets/$SR_HOST.p12 \
+-deststoretype $SSL_STORE_TYPE  \
+-srcstoretype $SSL_STORE_TYPE \
+-noprompt \
+-srcstorepass $P12_PASS
+
+# create sr trust store and ca cert
 keytool -keystore $SR_CA \
 -alias CARoot \
 -import \
